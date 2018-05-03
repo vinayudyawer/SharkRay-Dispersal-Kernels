060fba4de875e2387406dd50c16b3667ac4cafaf
Vinay Udyawer <vinay.udyawer@gmail.com>
2018-05-03 13:52:17 +1000

    update sat data

---
+++ R/StandardMetricCalculation.R
@@ -0,0 +1,12 @@
+## Using ATT pakage to calculate standard metrics for acoustic and satellite data
+
+#devtools::install_github("vinayudyawer/ATT")
+library(ATT)
+library(tidyverse)
+library(data.table)
+
+satdat<-fread("Data/Satellite data/2018-05-03_ATN_satData.csv")
+
+
+
+
