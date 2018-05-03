a4ceea7920cfc63493e42fc5533935b1d619d91a
creeas <cmull@sfu.ca>
2018-05-02 18:53:05 -0700

    stuff

---
+++ R/Talk_theme_ggplot.R
@@ -0,0 +1,11 @@
+#Create a custom Theme for ggplot2 for talks (typically I use a black background for my slides)
+theme_talk <- function() {
+  theme_light(base_size = 25, base_family = "") %+replace%
+    theme(panel.background = element_rect(fill = "transparent", colour = NA),
+          plot.background = element_rect(fill = "transparent", colour = NA),
+          legend.background = element_blank(),
+          panel.grid = element_blank(),
+          title = element_blank(),
+          legend.position = "none"
+          )
+}
